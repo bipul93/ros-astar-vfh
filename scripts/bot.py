88a19a4b581294f19c59aad24662bccb6dc29e44
bipul93 <kumar.bipulbose@gmail.com>
2019-10-12 22:26:14 -0400

    A star corrected

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
+from functools import reduce
 
 import rospy
-from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
 import enum
 import math
 import numpy
 import ast
+# from .vfh.histogram import Histogram
 
 
 # Defining Bot States
@@ -24,7 +24,6 @@ class BotState(enum.Enum):
 yaw = 0
 yaw_threshold = math.pi / 90
 goal_distance_threshold = 0.25
-currentBotState = BotState.LOOK_TOWARDS
 
 # base scan laser range values
 maxRange = 3
@@ -32,7 +31,6 @@ minRange = 0
 
 bot_pose = None
 init_bot_pose = []
-beacon_pose = None
 bot_motion = None  # cmd_vel publisher
 homing_signal = None  # subscriber
 init_config_complete = False
@@ -44,14 +42,13 @@ distance_moved = 0
 front_obs_distance = None
 left_obs_distance = None
 
-wall_folllowing = False
 
-# goalx = rospy.get_param('goalx')
-# goaly = rospy.get_param('goaly')
+goalx = rospy.get_param('goalx')
+goaly = rospy.get_param('goaly')
 mapMatrix = None
 
 
-class Block():
+class Node:
     def __init__(self, parent=None, position=None):
         self.parent = parent
         self.position = position
@@ -59,17 +56,51 @@ class Block():
         self.f = 0
         self.g = 0
         self.h = 0
+        self.diagonal = False
 
     def __eq__(self, other):
         return self.position == other.position
 
 
-def translation(map_to_simulation, row, column):
-    if map_to_simulation:
-        return {"y": 10 - row, "x": column - 9}
+class Histogram:
+    def __init__(self, bin_size):
+        self.bin_size = bin_size
+        self.bin_width = 360 / bin_size
+        self.histogram = [0] * bin_size
+
+    def build_histogram(self, sensor_data):
+        smoothed_histogram = [0] * self.bin_size
+        # histogram_data = []
+        # for index, item in enumerate(sensor_data):
+        #     histogram_data.append(math.fabs(item-3))
+        # print(histogram_data)
+
+        # sensor_data_length = len(sensor_data) 360 degrees. taking static constant
+        # obstacle condition. within 1 from range(0-3)
+        for bin_index in range(self.bin_size):
+            if bin_index == 0:
+                start_index = 0
+                end_index = 73
+            else:
+                start_index = (360/self.bin_size)*bin_index + 1
+                end_index = (360/self.bin_size)*(bin_index + 1) + 1
+
+            self.histogram[bin_index] = reduce(lambda count, i: count + (i < 2), sensor_data[start_index:end_index], 0)
+            print(start_index, end_index)
+            print(sensor_data[start_index:end_index])
+        print(self.histogram)
+
+
+histogram = Histogram(5)
+
+
+def translation(simulation_to_map, row, column):
+    if simulation_to_map:
+        return (10-row, column-9)
     else:
         # Here row and column value are actually x and y
-        return {"row": 10 - row, "column": column + 9}
+        # return {"row": 10 - row, "column": column + 9}
+        return (10 - row, column + 9)
 
 
 def process_map():
@@ -80,92 +111,103 @@ def process_map():
     print(mapMatrix)
 
     # ToDo: Find A* path
+    initx = -8.0
+    inity = -2.0
 
-    start_pos = (0, 0)
-    goal_pos = (7, 8)
+    goalx = 4.0
+    goaly = 9.0
+
+    start_pos = translation(False, int(math.floor(inity)), int(math.floor(initx)))
+    goal_pos = translation(False, int(math.floor(goaly)), int(math.floor(goalx)))
+    print(start_pos)
+    print(goal_pos)
     path = a_star_path(start_pos, goal_pos)
     print(path)
 
+    # for point in range(len(path)):
+    #     print(mapMatrix[path[point][0]][path[point][1]])
+
+
+def heuristic(start, goal):
+    return ((start[0] - goal[0]) ** 2) + ((start[1] - goal[1]) ** 2)
+
+
+def neighbours(node):
+    global mapMatrix
+    neighbours_list = []
+    # neighbour_nodes = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]  # 8-way movement
+    neighbour_nodes = [(0, -1), (0, 1), (-1, 0), (1, 0)]  # 4-way movement
+    for index, neighbour in enumerate(neighbour_nodes, start=1):
+        neighbour_pos = (node.position[0] + neighbour[0], node.position[1] + neighbour[1])
+
+        if neighbour_pos[0] > (len(mapMatrix) - 1) \
+                or neighbour_pos[0] < 0 \
+                or neighbour_pos[1] > (len(mapMatrix[len(mapMatrix) - 1]) - 1) \
+                or neighbour_pos[1] < 0:
+            continue
+
+        if mapMatrix[neighbour_pos[0]][neighbour_pos[1]] != 0:
+            continue
+
+        neighbour_node = Node(None, neighbour_pos)
+        if index > 4:
+            neighbour_node.g = node.g + 1.414  # diagonal_cost
+        else:
+            neighbour_node.g = node.g + 1  # normal cost
+        neighbour_node.parent = node
+        neighbours_list.append(neighbour_node)
+    return neighbours_list
+
 
 def a_star_path(start_pos, goal_pos):
-    global mapMatrix
-    open_pos_list = []
-    closed_pos_list = []
+    open_list = []
+    closed_list = []
 
-    # (f = g + h) //The idea here
+    start_node = Node(None, start_pos)
+    start_node.g = 0
+    start_node.f = start_node.g + heuristic(start_pos, goal_pos)
+    open_list.append(start_node)
 
-    start_block = Block(None, start_pos)
-    start_block.g = start_block.f = start_block.h = 0
-    open_pos_list.append(start_block)
+    goal_node = Node(None, goal_pos)
+    goal_node.g = 0
+    goal_node.f = goal_node.g + heuristic(start_pos, goal_pos)
 
-    goal_block = Block(None, goal_pos)
-    goal_block.g = goal_block.f = goal_block.h = 0
+    path = []
+    while len(open_list) > 0:
+        current_node = open_list[0]
+        current_list_index = 0
 
-    while len(open_pos_list) > 0:
-        current_block = open_pos_list[0]
-        current_index = 0
+        # open list element with lowest f score
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_list_index = index
 
-        for index, item in enumerate(open_pos_list):
-            if item.f < current_block.f:
-                current_block = item
-                current_index = index
+        # Goal found
+        if current_node == goal_node:
+            path_node = current_node
+            print(path_node.parent)
+            while path_node.parent is not None:
+                path.append(path_node.position)
+                path_node = path_node.parent
+            return path[::-1]
 
-        # Pop current off open list, add to closed list
-        open_pos_list.pop(current_index)
-        closed_pos_list.append(current_block)
+        open_list.pop(current_list_index)
+        closed_list.append(current_node)
 
-        # Found the goal
-        if current_block == goal_block:
-            path = []
-            current = current_block
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1]  # Return reversed path
+        neighbour_nodes = neighbours(current_node)
+        for neighbour in neighbour_nodes:
+            if neighbour not in closed_list:
+                neighbour.f = neighbour.g + heuristic(neighbour.position, goal_pos)
 
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:  # Adjacent squares
-
-            # Get node position
-            node_position = (current_block.position[0] + new_position[0], current_block.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (len(mapMatrix) - 1) or node_position[0] < 0 or node_position[1] > (
-                    len(mapMatrix[len(mapMatrix) - 1]) - 1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if mapMatrix[node_position[0]][node_position[1]] != 0:
-                continue
-
-            # Create new node
-            new_node = Block(current_block, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_pos_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_block.g + 1
-            child.h = ((child.position[0] - goal_block.position[0]) ** 2) + (
-                        (child.position[1] - goal_block.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_pos_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_pos_list.append(child)
+                if neighbour not in open_list:
+                    open_list.append(neighbour)
+                else:
+                    open_neighbour = open_list[open_list.index(neighbour)]
+                    if neighbour.g < open_neighbour.g:
+                        open_neighbour.g = neighbour.g
+                        open_neighbour.parent = neighbour.parent
+    return False
 
 
 def normalize(angle):
@@ -205,37 +247,17 @@ def get_base_truth(bot_data):
 
 
 def process_sensor_info(data):
+    # print(data.ranges)
+    histogram.build_histogram(data.ranges)
     global maxRange, minRange, front_obs_distance, left_obs_distance, zone_R, zone_FR, zone_F, zone_FL, zone_L
     maxRange = data.range_max
     minRange = data.range_min
 
-    # Note: Configuration one
-    # zone = numpy.array_split(numpy.array(data.ranges), 5)
-    # zone_R = zone[0]
-    # zone_FR = zone[1]
-    # zone_F = zone[2]
-    # zone_FL = zone[3]
-    # zone_L = zone[4]
-    # if front_obs_distance is None and left_obs_distance is None:
-    #     front_obs_distance = 0.75
-    #     left_obs_distance = 2
-
-    # Note: Configuration 2 - Breaking at uneven angles
-    zone = numpy.array(data.ranges)
-    zone_R = zone[0:50]  # 30deg
-    zone_FR = zone[51:140]
-    zone_F = zone[141:220]
-    zone_FL = zone[221:310]
-    zone_L = zone[311:361]
-    if front_obs_distance is None and left_obs_distance is None:
-        front_obs_distance = 1
-        left_obs_distance = 1
 
 
 def init():
-    global homing_signal
     rospy.init_node("bot", anonymous=True)
-    # rospy.Subscriber('/base_scan', LaserScan, process_sensor_info)
+    rospy.Subscriber('/base_scan', LaserScan, process_sensor_info)
     # rospy.Subscriber('/base_pose_ground_truth', Odometry, get_base_truth)
 
     rospy.spin()
@@ -244,7 +266,7 @@ def init():
 if __name__ == '__main__':
     try:
         process_map()
-        init()
+        # init()
     except rospy.ROSInterruptException:
         pass
 
